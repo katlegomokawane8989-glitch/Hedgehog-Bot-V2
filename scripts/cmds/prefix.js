0792d34656061c06ab471d4ab5778ccf39ac2805
katlegomokawane8989-glitch <katlegomokawane8989@gmail.com>
2026-01-03 01:32:56 +0200

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -45,7 +45,7 @@ module.exports = {
 			confirmThisThread: "Please react to this message to confirm change prefix in your box chat",
 			successGlobal: "Changed prefix of system bot to: %1",
 			successThisThread: "Changed prefix in your box chat to: %1",
-			myPrefix: "🌐 System prefix: %1\n🛸 Your box chat prefix: %2"
+			myPrefix: "💙sʏsᴛᴇᴍ ᴘʀᴇғɪx: %1\n🪽 ʏᴏᴜʀ ʙᴏᴛ ᴄʜᴀᴛ ᴘʀᴇғɪx: %2"
 		}
 	},
 
